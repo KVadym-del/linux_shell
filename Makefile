a13199f38b35ee6e3b80ae18a7aa3e6364face14
Vadym <vadymua@outlook.com>
2025-05-02 00:28:22 +0200

    Added first files.

---
+++ Makefile
@@ -0,0 +1,50 @@
+SHELLFLAGS = -Os -fno-ident -fno-asynchronous-unwind-tables -fno-stack-protector -fomit-frame-pointer -static -static-libstdc++
+INITFLAGS = -Os -fno-ident -fno-asynchronous-unwind-tables -fno-stack-protector -fomit-frame-pointer -static
+
+all: soft_clean build_iso
+
+shell.o: main.cpp
+	g++ -c ${SHELLFLAGS} -o shell.o main.cpp
+
+sys.o: sys.S
+	as sys.S -o sys.o
+
+init: shell.o sys.o
+	g++ -Os ${INITFLAGS} \
+    -Wl,--strip-all \
+	-Wl,-z,noexec \
+    shell.o sys.o -o init
+
+files: init lua
+	echo init >> files
+	echo lua >> files
+
+init.cpio: files
+	cat files | cpio -H newc -o > init.cpio
+
+build_iso: init.cpio
+	cp init.cpio ../linux/init.cpio; \
+	cd ../linux; \
+	make isoimage FDARGS="initrd=/init.cpio" FDINITRD=./init.cpio -j18
+
+run: build_iso
+	cd ../linux; \
+	qemu-system-x86_64 -cdrom arch/x86/boot/image.iso
+
+soft_clean:
+	rm -f init shell.o sys.o
+	rm -f *.o
+	rm -f *.a
+	rm -f *.so
+	rm -f *.out
+	rm -f *.bin
+	rm -f *.elf
+	rm -f *.cpio
+	rm -f ../linux/init.cpio
+	rm -f *.img
+	rm -f *.gz
+	rm -f files
+
+clean: soft_clean
+	cd ../linux; \
+	make clean;
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <cstring>
+#include <string>
+#include <sys/wait.h>
+#include <unistd.h>
+
+extern "C"
+{
+    int real_waitid(idtype_t idtype, id_t id, siginfo_t *infop, int options, void *);
+}
+
+constexpr const size_t PREALLOC_COMMAND_SIZE = 255;
+constexpr const char *PROMPT = "#> ";
+
+int main(void)
+{
+    std::string command{};
+    command.reserve(PREALLOC_COMMAND_SIZE);
+    while (true)
+    {
+        size_t res = write(1, PROMPT, std::strlen(PROMPT));
+        if (res == -1)
+        {
+            return 1;
+        }
+        res = read(0, command.data(), command.capacity());
+        if (res == -1)
+        {
+            return 1;
+        }
+        if (res > 0 && command[res - 1] == '\n')
+        {
+            command[res - 1] = '\0';
+        }
+        else
+        {
+            command[res] = '\0';
+        }
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            execve(command.c_str(), nullptr, nullptr);
+            break;
+        }
+        else
+        {
+            siginfo_t info{};
+            real_waitid(P_ALL, 0, &info, WEXITED, 0);
+        }
+    }
+
+    _exit(0);
+}
