6c86025d6591ca58938580184705e433f6255f77
Vadym <vadymua@outlook.com>
2025-05-02 00:47:35 +0200

    Some code style changes.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 extern "C"
 {
-    int real_waitid(idtype_t idtype, id_t id, siginfo_t *infop, int options, void *);
+    int real_waitid(idtype_t idtype, id_t id, siginfo_t* infop, int options, void*);
 }
 
 constexpr const size_t PREALLOC_COMMAND_SIZE = 255;
-constexpr const char *PROMPT = "#> ";
+constexpr const char* PROMPT = "#> ";
 
 int main(void)
 {
@@ -19,22 +19,17 @@ int main(void)
     {
         size_t res = write(1, PROMPT, std::strlen(PROMPT));
         if (res == -1)
-        {
             return 1;
-        }
+
         res = read(0, command.data(), command.capacity());
         if (res == -1)
-        {
             return 1;
-        }
+
         if (res > 0 && command[res - 1] == '\n')
-        {
             command[res - 1] = '\0';
-        }
         else
-        {
             command[res] = '\0';
-        }
+
         pid_t pid = fork();
         if (pid == 0)
         {
@@ -44,7 +39,7 @@ int main(void)
         else
         {
             siginfo_t info{};
-            real_waitid(P_ALL, 0, &info, WEXITED, 0);
+            real_waitid(P_ALL, 0, &info, WEXITED, nullptr);
         }
     }
 
